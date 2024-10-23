4e31ea4ee137be20dec0c6ae2aef45dfe80a3449
Adityasundarka <adityakumarsundarka1@gmail.com>
2024-10-23 22:04:27 +0530

    add new file

---
+++ frontend/src/Component/Tn/Tn.jsx
@@ -0,0 +1,13 @@
+
+
+
+const Tn =() => {
+    return(
+        <>
+            <h1>Sagar</h1>
+        </>
+    )
+}
+
+
+export default Tn;
--- frontend/src/Pages/Left/Left.jsx
+++ frontend/src/Pages/Left/Left.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Left.css'
 import Follow from '../../Component/LeftComponent/Follow';
+import Tn from '../../Component/Tn/Tn';
 
 const Left = () => {
   return (
     <div className='left'>
       <Follow />
+      <Tn />
     </div>
   )
 }
